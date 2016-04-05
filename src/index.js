f95455482e1d37583a5a2d12b747b345435ff988
Benjamin Charity <ben@benjamincharity.com>
2016-04-05 19:52:05 -0400

    namespaced service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { CalendarService } from './calendar.service';
 
 export default angular.module('bc.JsonCalendar', [])
-    .service('CalendarService', CalendarService)
+    .service('bcCalendarService', CalendarService)
 ;
 
