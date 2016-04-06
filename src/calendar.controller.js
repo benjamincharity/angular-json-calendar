f08775c7106cb7fe0d847bead347cdc7f6400496
Benjamin Charity <ben@benjamincharity.com>
2016-04-06 11:43:21 -0400

    isBeforeToday now working, added in service

--- src/calendar.controller.js
+++ src/calendar.controller.js
@@ -1,11 +1,12 @@
 export class CalendarController {
 
     constructor(
-        bcCalendarConfig
+        bcCalendarConfig, bcCalendarService
     ) {
         'ngInject';
 
         this.bcCalendarConfig = bcCalendarConfig;
+        this.bcCalendarService = bcCalendarService;
 
         console.log('config: ', bcCalendarConfig);
 
@@ -17,10 +18,42 @@ export class CalendarController {
 
 
     _activate() {
+        // Define the starting day of the calendar
         this.startDate = this.startDate || this.bcCalendarConfig.startDate;
 
         console.log('startDate: ', this.startDate);
 
+
+    }
+
+
+    /**
+     * Check to see if the day is prior to the current date
+     * This is used to disable the unselectable days
+     * TODO: Can I really not just compare dates?
+     *
+     * @param {Date} day
+     * @return {Bool} isBefore
+     */
+    isBeforeToday(date) {
+        const dateDayOfMonth = moment(date).date();
+        const doMonthsMatch = this.bcCalendarService.doMonthsMatch(this.startDate, date);
+        const todayOfMonth = moment(this.startDate).date();
+
+        // If both days are in the same month and the passed day comes earlier than today
+        return (doMonthsMatch && (dateDayOfMonth < todayOfMonth));
+    }
+
+
+    isSelectedDay() {
+    }
+
+
+    isToday() {
+    }
+
+
+    selectDate() {
     }
 
 }
