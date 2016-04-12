9ef988f12b3707346b2ab6709396aab536914bf0
Benjamin Charity <ben@benjamincharity.com>
2016-04-12 17:25:53 -0400

    import week and month directives

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { bcCalendarConfig } from './calendar.provider';
 import { bcCalendarService } from './calendar.service';
 import { bcCalendarDirective } from './calendar.directive';
+import { bcMonthDirective } from './month.directive';
+import { bcWeekDirective } from './week.directive';
 import { bcDayDirective } from './day.directive';
 
 export default angular.module('bc.JsonCalendar', [])
     .provider('bcCalendarConfig', bcCalendarConfig)
     .service('bcCalendarService', bcCalendarService)
     .directive('bcCalendar', bcCalendarDirective)
+    .directive('bcMonth', bcMonthDirective)
+    .directive('bcWeek', bcWeekDirective)
     .directive('bcDay', bcDayDirective)
 ;
 
