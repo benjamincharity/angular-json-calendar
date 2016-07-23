7c279ae170994b59804bd95c04ff2ca13556b2eb
Benjamin Charity <ben@benjamincharity.com>
2016-07-23 15:46:38 -0400

    created first service test

---
+++ tests/calendar.service.spec.js
@@ -0,0 +1,21 @@
+describe('bcCalendarService', () => {
+    let bcCalendarService;
+
+    // Include the module
+    beforeEach(module('bc.JsonCalendar'));
+
+    // Inject the service
+    beforeEach(inject((_bcCalendarService_) => {
+        bcCalendarService = _bcCalendarService_;
+    }));
+
+    describe('dateIsBeforeToday', () => {
+        const pastDate = '2016-05-01T00:00:00.027Z';
+
+        it('should verify that the date is before today', () => {
+            expect(bcCalendarService.dateIsBeforeToday(pastDate)).toEqual(true);
+        });
+
+    });
+});
+
