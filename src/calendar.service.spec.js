733568186dc15dfc7be4d20c6503ee1d4390a277
Benjamin Charity <ben@benjamincharity.com>
2016-07-23 17:37:58 -0400

    added test for isDayToday

--- src/calendar.service.spec.js
+++ src/calendar.service.spec.js
@@ -9,6 +9,8 @@ describe('bcCalendarService', () => {
         bcCalendarService = _bcCalendarService_;
     }));
 
+
+
     describe('dateIsBeforeToday', () => {
         const pastDate = '2016-05-01T00:00:00.027Z';
 
@@ -17,5 +19,18 @@ describe('bcCalendarService', () => {
         });
 
     });
+
+
+    describe('isDayToday', () => {
+        const date1 = moment('2013-02-08').format();
+        const date2 = moment('2013-02-08').format();
+
+        it('should verify that the date is today', () => {
+            expect(bcCalendarService.isDayToday(date1, date2)).toEqual(true);
+        });
+
+    });
+
+
 });
 
