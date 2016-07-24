e90530395670b2642fd7cac6a1a03619e497a037
Benjamin Charity <ben@benjamincharity.com>
2016-07-24 01:34:55 -0400

    added integerToArray and padDaysLeft tests

--- src/calendar.service.spec.js
+++ src/calendar.service.spec.js
@@ -15,7 +15,7 @@ describe('bcCalendarService', () => {
         const pastDate = '2016-05-01T00:00:00.027Z';
 
         it('should verify that the date is before today', () => {
-            expect(bcCalendarService.dateIsBeforeToday(pastDate)).toEqual(true);
+            expect(bcCalendarService.dateIsBeforeToday(pastDate)).toBe(true);
         });
 
     });
@@ -26,7 +26,49 @@ describe('bcCalendarService', () => {
         const date2 = moment('2013-02-08').format();
 
         it('should verify that the date is today', () => {
-            expect(bcCalendarService.isDayToday(date1, date2)).toEqual(true);
+            expect(bcCalendarService.isDayToday(date1, date2)).toBe(true);
+        });
+
+    });
+
+
+    describe('integerToArray', () => {
+        const ARRAY_LENGTH = 3;
+        let array;
+
+        beforeEach(() => {
+            array = bcCalendarService.integerToArray(ARRAY_LENGTH);
+        });
+
+        it('should be an array the length of ARRAY_LENGTH', () => {
+            expect(array.length).toEqual(ARRAY_LENGTH);
+        });
+
+    });
+
+
+    describe('padDaysLeft', () => {
+        const COUNT = 3;
+        const START = moment('2016-11-05').format();
+        let pad;
+
+        beforeEach(() => {
+            pad = bcCalendarService.padDaysLeft(START, COUNT);
+        });
+
+        afterEach(() => {
+            pad = null;
+        });
+
+        it('should be an array of days equal to COUNT', () => {
+            expect(pad.length).toEqual(COUNT);
+        });
+
+        it('should be ordered from earliest to latest', () => {
+            const lastDay = pad[pad.length - 1].date;
+            const secondToLastDay = pad[pad.length - 2].date;
+
+            expect(moment(secondToLastDay).isBefore(lastDay)).toBe(true);
         });
 
     });
