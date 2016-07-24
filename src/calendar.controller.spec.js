3ebafec68bf0ce06325e9c2bbf6c20a1609e127c
Benjamin Charity <ben@benjamincharity.com>
2016-07-24 19:27:18 -0400

    added first controller test

--- src/calendar.controller.spec.js
+++ src/calendar.controller.spec.js
@@ -1,5 +1,4 @@
 describe('CalendarController', () => {
-
     let $compile;
     let $rootScope;
 
@@ -11,5 +10,34 @@ describe('CalendarController', () => {
         $compile = _$compile_;
         $rootScope = _$rootScope_;
     }));
+
+
+    describe('bcDays are used', () => {
+        let $scope;
+        let element;
+        let vm;
+        const THREE_DAYS = 3;
+
+        beforeEach(() => {
+            $scope = $rootScope.$new();
+            element = angular.element(
+                `<bc-calendar bc-days="${THREE_DAYS}" bc-nesting-depth="day"></bc-calendar>`
+            );
+            element = $compile(element)($scope);
+            $scope.$apply();
+            vm = element.isolateScope().vm;
+        });
+
+        it('should be set to create three days', () => {
+            expect(vm.days).toEqual(THREE_DAYS);
+        });
+
+        it('should have generated DOM for the days', () => {
+            expect(element.find('span')[0]).toBeDefined();
+        });
+
+
+
+    });
 
 });
